b6aff4e81762796fa34c5f83ccb5d747576ec857
FND <fnd@localhost.localdomain>
2013-04-24 08:25:52 +0200

    created tiddler serialization
    very much incomplete - possibly not even valid RDF/Turtle

---
+++ test/test_turtle.py
@@ -0,0 +1,33 @@
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.serializer import Serializer
+from tiddlyweb.config import config
+
+from tiddlywebplugins.rdf import init
+
+
+def setup_module(module):
+    config['server_host'] = {
+        'scheme': 'http',
+        'host': 'example.org',
+        'port': '80',
+    }
+    config['server_prefix'] = '/wiki'
+    module.ENVIRON = { 'tiddlyweb.config': config }
+
+    init(config)
+
+
+def test_entity():
+    tiddler = Tiddler('Hello World', 'alpha')
+
+    serializer = Serializer('tiddlywebplugins.rdf', ENVIRON)
+    serializer.object = tiddler
+    representation = serializer.to_string()
+
+    assert representation == '''
+@prefix host: <http://example.org/wiki/>.
+@prefix tweb: <http://tiddlyweb.com/>.
+
+:bags/alpha/Hello%20World a tweb:tiddler;
+    tweb:bag :bags/alpha
+    '''.strip()
--- tiddlywebplugins/rdf/__init__.py
+++ tiddlywebplugins/rdf/__init__.py
@@ -8,3 +8,19 @@ def init(config):
     config['serializers'][content_type] = [__package__,
             '%s; charset=UTF-8' % content_type]
     config['extension_types']['turtle'] = content_type
+
+
+class Serialization(SerializationInterface):
+
+  def tiddler_as(self, tiddler):
+    host = '%s%s/' % (server_host_url(self.environ),
+        self.environ['tiddlyweb.config']['server_prefix']) # TODO: normalize -- XXX: reinventing the wheel?
+
+    title, bag = [encode_name(name) for name in (tiddler.title, tiddler.bag)]
+    return """
+@prefix host: <%s>.
+@prefix tweb: <http://tiddlyweb.com/>.
+
+:bags/%s/%s a tweb:tiddler;
+    tweb:bag :bags/%s
+    """.strip() % (host, bag, title, bag)
